d74e631db6062d5a7d2f5a10558aa91cdb6458b2
Ethan Turkeltaub <ethan.turkeltaub@gmail.com>
2012-09-16 20:47:01 -0400

    book example for source.

--- models/source.rb
+++ models/source.rb
@@ -7,12 +7,43 @@ class Source
   belongs_to :project
 
   many :comments, :as => :commentable
+
+  many :contributors
+  one :annotation
 end
 
-class Book
+class Book < Source
+  key :title, String
+  key :city, String
+  key :publisher, String
+  key :year, Date
+  key :page_numbers, Boolean
+  key :scholarly_edition, Boolean
+  key :edition, String
+  key :number_volumes, Integer
+  key :volume, Integer
+  key :title_multivolume, String
+  key :series_name, String
+  key :series_number, String
+  key :original_year, Date
 end
 
-class Website
+class Contributor
+  include MongoMapper::EmbeddedDocument
+
+  key :first, String
+  key :middle, String
+  key :last, String
+  key :suffix, String
+  key :role, String
+
+  embedded_in :source
+end
+
+class Annotation
+  include MongoMapper::EmbeddedDocument
+
+  key :text, String
 end
 
 # Completely incomplete.
--- spec/unit/models/comment_spec.rb
+++ spec/unit/models/comment_spec.rb
@@ -1,7 +1,7 @@
 require "spec_helper"
 
 describe Comment do
-  let(:note){ Note.create(:title => "Foobar", :comments => [ { "text" => "Foo to the bar" } ] ) }
+  let(:note){ Note.create(:title => "Foobar", :comments => [ { "text" => "Foo to the bar" } ] ) } # Use factories in the future.
   
   it "can be instantiated" do
     note.comments.should_not be_blank
