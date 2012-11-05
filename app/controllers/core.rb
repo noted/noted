a8371a0d8e29bae0f0026f97e12ad04163115f74
Ethan Turkeltaub <ethan.turkeltaub@gmail.com>
2012-11-04 21:34:17 -0500

    more work on index and footer. begun dashboard. wrote index/dashboard specs.

--- app/controllers/core.rb
+++ app/controllers/core.rb
@@ -2,4 +2,8 @@ Noted.controllers do
   get :index do
   	render "core/index"
   end
+
+  get :dashboard do
+  	render "core/dashboard"
+  end
 end
--- spec/unit/app/controllers/core_controller_spec.rb
+++ spec/unit/app/controllers/core_controller_spec.rb
@@ -1,15 +1,23 @@
 require 'spec_helper'
 
 describe "Core" do
-  describe "(deauthenticated)" do
-    describe "GET /" do
-      before do
-        get "/"
-      end
+  describe "GET /" do
+    before do
+      get "/"
+    end
 
-      it "should be ok" do
-        response.should be_ok
-      end
+    it "should be ok" do
+      response.should be_ok
     end
   end
+
+  describe "GET /dashboard" do
+    before do
+      get "/dashboard"
+    end
+
+    it "should be ok" do
+      response.should be_ok
+    end
+  end 
 end
