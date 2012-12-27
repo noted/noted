18317bc8b8c7dae77467d0879a69b92ec5776d06
Ethan Turkeltaub <ethan.turkeltaub@gmail.com>
2012-12-26 23:37:01 -0500

    cleaned projects controller and specs.

--- app/controllers/projects.rb
+++ app/controllers/projects.rb
@@ -4,11 +4,12 @@ Noted.controllers :projects do
   end
 
   post :create do
-    u = User.find(params[:user_id])
-    p = Project.new(params[:id])
+    u = User.find(params[:user])
+
+    p = Project.new(params[:project])
+    p.creator = User.find(params[:author])
 
     if p.save && u.projects << p
-      flash[:notice] = "#{p.title} has been created."
       redirect url(:projects, :view, :user => u.username, :project => p.permalink)
     else
       flash[:error] = "Something has gone awry."
@@ -18,36 +19,36 @@ Noted.controllers :projects do
 
   get :view, :map => "/:user/:project" do
     @user = User.find_by_username(params[:user])
-    @project = @user.projects.find_by_permalink(params[:project])
+    @project = @user.projects.find_by_permalink(params[:permalink])
 
     render 'projects/view'
   end
 
   get :edit, :map => "/:user/:project/settings" do
     @user = User.find_by_username(params[:user])
-    @project = @user.projects.find_by_permalink(params[:project])
+    @project = @user.projects.find_by_permalink(params[:permalink])
 
     render 'projects/edit'
   end
 
   patch :update do
     p = Project.find(params[:id])
+    p.updater = User.find(params[:author])
 
     if p.update_attributes(params[:project])
-      flash[:notice] = "#{p.title} has been modified."
-      redirect url(:projects, :edit, :user => p.user.username, :project => p.permalink)
+      redirect url(:projects, :view, :user => p.user.username, :project => p.permalink)
     else
       flash[:error] = "Something has gone awry."
-      redirect url(:projects, :edit, :user => params[:user_id], :project => p.permalink)
+      redirect url(:project, :edit, :user => p.user.username, :project => p.permalink)
     end
   end
 
   delete :destroy do
     p = Project.find(params[:id])
+    u = p.user
 
     if p.destroy
-      flash[:notice] = "Your project has been destroyed."
-      redirect url(:users, :view, :user => User.find(params[:user]).username)
+      redirect url(:users, :view, :user => u.username)
     else
       flash[:error] = "Something has gone awry."
       redirect url(:projects, :edit, :user => p.user.username, :project => p.permalink)
--- spec/unit/app/controllers/projects_controller_spec.rb
+++ spec/unit/app/controllers/projects_controller_spec.rb
@@ -4,14 +4,6 @@ describe "Projects" do
   let(:project) { create(:project) }
   let(:user)    { project.user }
 
-  describe "GET /:username/:project" do
-    before do
-      get "/#{user.username}/#{project.permalink}"
-    end
-
-    it { response.should be_ok }
-  end
-
   describe "GET /projects/new" do
     before do
       get "/projects/new"
@@ -25,13 +17,24 @@ describe "Projects" do
       post "/projects/create", :user => user.id, :author => user.id, :project => { :title => "The Hubble Telescope" }
     end
 
-    context "redirects" do
+    describe "redirects" do
       it { response.should be_redirect }
       it { response.location.should include("/#{user.username}/the-hubble-telescope") }
     end
 
-    it { Project.find_by_title("The Hubble Telescope").should_not be_nil }
-    it { user.projects.should include(Project.find_by_title("The Hubble Telescope")) }
+    describe "database" do
+      it { Project.find_by_title("The Hubble Telescope").should_not be_nil }
+      it { Project.find_by_title("The Hubble Telescope").creator.should eql(user) }
+      it { user.projects.should include(Project.find_by_title("The Hubble Telescope")) }
+    end
+  end
+
+  describe "GET /:username/:project" do
+    before do
+      get "/#{user.username}/#{project.permalink}"
+    end
+
+    it { response.should be_ok }
   end
 
   describe "GET /:username/:project/settings" do
@@ -49,24 +52,29 @@ describe "Projects" do
       project.reload
     end
 
-    context "redirects" do
+    describe "redirects" do
       it { response.should be_redirect }
-      it { response.location.should include("#{project.user.username}/#{project.permalink}/settings") }
+      it { response.location.should include("#{project.user.username}/#{project.permalink}") }
     end
 
-    it { project.title.should eql("Large Hadron Collider") }
+    describe "database" do
+      it { project.title.should eql("Large Hadron Collider") }
+      it { project.updater.should eql(user) }
+    end
   end
 
   describe "DELETE /projects/destroy" do
     before do
-      delete "/projects/destroy", :id => project.id, :author => user.id
+      delete "/projects/destroy", :id => project.id
     end
 
-    context "redirects" do
+    describe "redirects" do
       it { response.should be_redirect }
       it { response.location.should include("/#{project.user.username}") }
     end
 
-    it { Project.find_by_name("The Cosmos").should be_nil }
+    describe "database" do
+      it { Project.find_by_name("The Cosmos").should be_nil }
+    end
   end
 end
