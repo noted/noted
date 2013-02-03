692f84bfc3d8f88679adc4ed9db131fc60aa4d4a
Ethan Turkeltaub <ethan.turkeltaub@gmail.com>
2013-02-02 21:36:08 -0500

    new project ui.

--- app/controllers/projects.rb
+++ app/controllers/projects.rb
@@ -19,7 +19,7 @@ Noted.controllers :projects do
 
   get :view, :map => "/:user/:project" do
     @user = User.find_by_username(params[:user])
-    @project = Project.where(:user_id => @user.id, :permalink => params[:project]).first
+    @project = Project.where(:user_id => @user.id, :permalink => params[:project]).order("updated_at dsc").first
 
     render 'projects/view'
   end
