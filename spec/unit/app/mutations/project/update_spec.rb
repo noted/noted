e04653153c38aeab7f67c88c6c2e78b171b07f6c
Ethan Turkeltaub <ethan.turkeltaub@gmail.com>
2013-03-19 22:10:47 -0400

    specs for Project::Update.

---
+++ spec/unit/app/mutations/project/update_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+
+describe Project::Update do
+  let(:project) { create(:project) }
+  let(:user)    { project.user }
+
+  let(:action) do
+    Project::Update.run({
+      :project => project.id,
+      :author => user.id,
+      :title => "Hayden Planetarium",
+      :description => "Foobar."
+    })
+  end
+
+  before do
+    project.reload
+  end
+
+  it { action.success?.should be_true }
+  it { action.result.should be_an_instance_of Project }
+  it { action.result.title.should eql "Hayden Planetarium" }
+  it { action.result.description.should eql "Foobar." }
+  it { action.result.updater.should eql user }
+end
