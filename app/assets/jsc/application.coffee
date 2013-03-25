4bf2c76b41418d911a712017eb0fa8b1f3ee956f
Ethan Turkeltaub <ethan.turkeltaub@gmail.com>
2013-03-25 08:14:04 -0400

    make project description a dropdown.

--- app/assets/jsc/application.coffee
+++ app/assets/jsc/application.coffee
@@ -1,38 +1,8 @@
 $(document).ready ->
   $("#user aside h2").fitText 1.1
-  #$('textarea').autoResize();
 
-  opts = {
-    container: 'epiceditor',
-    basePath: '/jsc/lib/epiceditor/epiceditor',
-    file: {
-      name: 'epiceditor',
-      defaultContent: '',
-      autoSave: 100
-    },
-    theme: {
-      base:'/jsc/lib/epiceditor/epiceditor/themes/base/epiceditor.css'
-    }
-  }
-
-  editor = new EpicEditor(opts)
-
-  updateEditorHeight = ->
-    editorHeight = $(editor.getElement("editor").body).height()
-
-    # +20 for padding
-    $("#epiceditor").height editorHeight + 20
-    editor.reflow()
-
-  editor.load ->
-    updateEditorHeight()
-
-  editor.on "update", ->
-
-    # You should probably put a check here so it doesn't
-    # run for every update, but just on update, AND if the
-    # element's height is different then before.
-    updateEditorHeight()
-
-  $("a.preview").onclick = ->
-    editor.preview()
+  $(".description h4 a.expand").on "click", ->
+    if $(".description .text").hasClass("hidden")
+      $(".description .text").removeClass("hidden").addClass("display")
+    else
+      $(".description .text").removeClass("display").addClass "hidden"
--- public/jsc/application.js
+++ public/jsc/application.js
@@ -1,36 +1,14 @@
 (function() {
 
   $(document).ready(function() {
-    var editor, opts, updateEditorHeight;
     $("#user aside h2").fitText(1.1);
-    opts = {
-      container: 'epiceditor',
-      basePath: '/jsc/lib/epiceditor/epiceditor',
-      file: {
-        name: 'epiceditor',
-        defaultContent: '',
-        autoSave: 100
-      },
-      theme: {
-        base: '/jsc/lib/epiceditor/epiceditor/themes/base/epiceditor.css'
+    return $(".description h4 a.expand").on("click", function() {
+      if ($(".description .text").hasClass("hidden")) {
+        return $(".description .text").removeClass("hidden").addClass("display").slide;
+      } else {
+        return $(".description .text").removeClass("display").addClass("hidden");
       }
-    };
-    editor = new EpicEditor(opts);
-    updateEditorHeight = function() {
-      var editorHeight;
-      editorHeight = $(editor.getElement("editor").body).height();
-      $("#epiceditor").height(editorHeight + 20);
-      return editor.reflow();
-    };
-    editor.load(function() {
-      return updateEditorHeight();
     });
-    editor.on("update", function() {
-      return updateEditorHeight();
-    });
-    return $("a.preview").onclick = function() {
-      return editor.preview();
-    };
   });
 
 }).call(this);
