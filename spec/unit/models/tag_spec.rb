0d60dbbbf5d3e7c26b658fb049473306ce684f6b
Ethan Turkeltaub <ethan.turkeltaub@gmail.com>
2013-03-26 17:53:22 -0400

    more thorough Tag specs.

--- spec/unit/models/tag_spec.rb
+++ spec/unit/models/tag_spec.rb
@@ -1,13 +1,15 @@
 require 'spec_helper'
 
 describe Tag do
-  let(:tag) { create(:tag) }
-  let(:note) do
-    n = create(:note)
-    n.tags << tag
-    n
+  let(:tag)  { create(:tag) }
+  let(:note) { create(:note) }
+
+  before do
+    note.tags << tag
+    note.save
   end
 
   it { tag.should be_valid }
+  it { tag.taggable.should eql note }
   it { note.tags.should include tag }
 end
