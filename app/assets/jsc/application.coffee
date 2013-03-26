2f470476c4c5b0b90abd4c4ba331b7f8ad900f6b
Ethan Turkeltaub <ethan.turkeltaub@gmail.com>
2013-03-26 14:42:27 -0400

    trying to get tags to sort (#16).

--- app/assets/jsc/application.coffee
+++ app/assets/jsc/application.coffee
@@ -1,3 +1,7 @@
+jQuery.removeFromArray = (value, arr) ->
+  jQuery.grep arr, (elem, index) ->
+    elem isnt value
+
 $(document).ready ->
   $("#user aside h2").fitText 1.1
 
@@ -10,16 +14,50 @@ $(document).ready ->
   $('input.tags').tagsManager
     hiddenTagListName: 'note[tags]'
 
-  arr = $('input[name="existing-tags"]').attr('value').split(",")
-  for i of arr
-    $('input.tags').tagsManager('pushTag', arr[i])
+  arr = $('input[name="existing-tags"]').attr('value')
+
+  if arr
+    arr = arr.split(",")
+    for i of arr
+      $('input.tags').tagsManager('pushTag', arr[i])
 
   $("input.tags").autoGrowInput
     comfortZone: 20
     minWidth: 200
     maxWidth: 900
 
-
   $(".textbox footer").click ->
     $(".textbox footer input").focus()
 
+
+  tags = []
+
+  # If tag in note is clicked
+  $("footer ul.tags li a").on 'click', ->
+    # Unless the tag is already in the array
+    unless tags.indexOf($(this).text()) != -1
+      # Add it to the array
+      tags.push($(this).text())
+
+      # Put it in the header
+      $("header ul.tags").append '<li data-value="' + $(this).text() + '"><span class="btn">' + $(this).text() + ' <a href="#"><i class="ss-icon">close</i></a></span></li>'
+
+    console.log tags
+
+  # If tag in header is clicked
+  $("header ul.tags a").on 'click', ->
+    console.log "go"
+
+    # Remove from array
+    $.removeFromArray(($(this).attr("data-value")), tags)
+
+    console.log tags
+
+    # Remove from header list
+    $(this).parent().parent().remove()
+
+  $("header ul.tags li span").on 'click', "header ul.tags li span", ->
+    console.log "foo"
+
+  $("a").on "click", ->
+    console.log "fo"
--- public/jsc/application.js
+++ public/jsc/application.js
@@ -1,7 +1,13 @@
 (function() {
 
+  jQuery.removeFromArray = function(value, arr) {
+    return jQuery.grep(arr, function(elem, index) {
+      return elem !== value;
+    });
+  };
+
   $(document).ready(function() {
-    var arr, i;
+    var arr, i, tags;
     $("#user aside h2").fitText(1.1);
     $(".description h4 a.expand").on("click", function() {
       if ($(".description .text").hasClass("hidden")) {
@@ -13,17 +19,40 @@
     $('input.tags').tagsManager({
       hiddenTagListName: 'note[tags]'
     });
-    arr = $('input[name="existing-tags"]').attr('value').split(",");
-    for (i in arr) {
-      $('input.tags').tagsManager('pushTag', arr[i]);
+    arr = $('input[name="existing-tags"]').attr('value');
+    if (arr) {
+      arr = arr.split(",");
+      for (i in arr) {
+        $('input.tags').tagsManager('pushTag', arr[i]);
+      }
     }
     $("input.tags").autoGrowInput({
       comfortZone: 20,
       minWidth: 200,
       maxWidth: 900
     });
-    return $(".textbox footer").click(function() {
+    $(".textbox footer").click(function() {
       return $(".textbox footer input").focus();
+    });
+    tags = [];
+    $("footer ul.tags li a").on('click', function() {
+      if (tags.indexOf($(this).text()) === -1) {
+        tags.push($(this).text());
+        $("header ul.tags").append('<li data-value="' + $(this).text() + '"><span class="btn">' + $(this).text() + ' <a href="#"><i class="ss-icon">close</i></a></span></li>');
+      }
+      return console.log(tags);
+    });
+    $("header ul.tags a").on('click', function() {
+      console.log("go");
+      $.removeFromArray($(this).attr("data-value"), tags);
+      console.log(tags);
+      return $(this).parent().parent().remove();
+    });
+    $("header ul.tags li span").on('click', "header ul.tags li span", function() {
+      return console.log("foo");
+    });
+    return $("a").on("click", function() {
+      return console.log("fo");
     });
   });
 
