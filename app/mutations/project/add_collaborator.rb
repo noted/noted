566c24049f0ad81538561ce851c1569dd66141a3
Ethan Turkeltaub <ethan.turkeltaub@gmail.com>
2013-05-08 21:15:45 -0400

    work on add_collaborator mutation.

--- app/mutations/project/add_collaborator.rb
+++ app/mutations/project/add_collaborator.rb
@@ -1,4 +1,24 @@
 class Project
   class AddCollaborator < Mutations::Command
+    required do
+      hash :project do
+        string :id
+      end
+
+      hash :collaborator do
+        string :id
+      end
+    end
+
+    def execute
+      p = Project.find(project[:id])
+
+      u = User.find(collaborator[:id])
+
+      p.collaborator_ids << u.id.to_s
+      p.updater = u
+
+      p.save
+    end
   end
 end
