399b0687ea18cd9566d04447658e8c3aef7a7c45
Ethan Turkeltaub <ethan.turkeltaub@gmail.com>
2013-05-10 23:54:27 -0400

    barebones specs for user search api.

--- spec/unit/api/controllers/users_controller_spec.rb
+++ spec/unit/api/controllers/users_controller_spec.rb
@@ -1,4 +1,13 @@
 require 'spec_helper'
 
-describe 'Users' do
+describe 'Users (API)' do
+  let(:user) { create(:user) }
+
+  describe 'GET /api/users/search.json' do
+    before do
+      get '/api/users/search.json?query=n'
+    end
+
+    it { response.should be_ok }
+  end
 end
