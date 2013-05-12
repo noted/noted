723fe4bc712249600aca47c0c03de01af81ade05
Ethan Turkeltaub <ethan.turkeltaub@gmail.com>
2013-05-11 22:37:52 -0400

    api for updating notes.

---
+++ api/controllers/notes.rb
@@ -0,0 +1,32 @@
+Noted::API.controllers :notes, :provides => :json do
+  patch :update do
+    n = Note::Update.run({
+      :author => params[:author],
+      :note => params[:note]
+    })
+
+    if n.success?
+      {
+        :status => 200,
+        :result => {
+          :note => {
+            :id => n.result.id.to_s,
+            :title => n.result.title,
+            :body => n.result.body,
+            :tags => n.result.tags_str,
+            :sources => n.result.sources
+          }
+        }
+      }.to_json
+    else
+      halt 400
+
+      {
+        :status => 400,
+        :errors => [
+          "Something went wrong"
+        ]
+      }.to_json
+    end
+  end
+end
