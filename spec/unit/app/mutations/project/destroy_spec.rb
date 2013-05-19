879da00979a38503f192aa915d38b197e969d12a
Ethan Turkeltaub <ethan.turkeltaub@gmail.com>
2013-05-19 16:14:15 -0400

    log updater when project is destroyed (resolves #103).

--- spec/unit/app/mutations/project/destroy_spec.rb
+++ spec/unit/app/mutations/project/destroy_spec.rb
@@ -2,9 +2,11 @@ require 'spec_helper'
 
 describe Project::Destroy do
   let(:project) { create(:project) }
+  let(:user)    { project.user }
 
   let(:action) do
     Project::Destroy.run({
+      :author => user.id.to_s,
       :project => {
         :id => project.id.to_s,
       }
