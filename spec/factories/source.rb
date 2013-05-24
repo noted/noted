167e7df41f2c15b64596a0ef3e571596bd79bb66
Ethan Turkeltaub <ethan.turkeltaub@gmail.com>
2013-05-24 16:16:34 -0400

    better source factory.

--- spec/factories/source.rb
+++ spec/factories/source.rb
@@ -1,21 +1,25 @@
 FactoryGirl.define do
   factory(:source) do
-    citation do
-      Scholar::Citation.new({
-        :type => 'book',
-        :title => 'The Hitchhiker\'s Guide to the Galaxy',
-        :contributors => [
-          {
-            :role => :author,
-            :first => 'Douglas',
-            :last => 'Adams'
-          }
-        ],
-        :publisher => 'Ballantine',
-        :city => 'New York',
-        :year => '2005'
-      })
+    ignore do
+      citation do
+        Scholar::Citation.new({
+          :type => 'book',
+          :title => 'The Hitchhiker\'s Guide to the Galaxy',
+          :contributors => [
+            {
+              :role => :author,
+              :first => 'Douglas',
+              :last => 'Adams'
+            }
+          ],
+          :publisher => 'Ballantine',
+          :city => 'New York',
+          :year => '2005'
+        })
+      end
     end
+
+    initialize_with { Source.new(:citation => citation) }
 
     project { |project| project.association(:project) }
   end
