b670e2e9f03b0db4831aae1db526ead2af1fe582
Ethan Turkeltaub <ethan.turkeltaub@gmail.com>
2014-03-10 13:15:43 -0400

    404 for not found users.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,12 +12,23 @@ class UsersController < ActionController::Base
 
   def show
     @user = User.where(username: params[:user]).first
-    @projects = @user.all_projects.sort('updated_at desc')
+
+    if @user
+      @projects = @user.all_projects.sort('updated_at desc')
+    else
+      not_found
+    end
   end
 
   def update
   end
 
   def destroy
+  end
+
+  protected
+
+  def not_found
+    render 'errors/404', status: 404
   end
 end
