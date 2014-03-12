22c05ff46d81f8b7badaf5747a832955d3191081
Ethan Turkeltaub <ethan.turkeltaub@gmail.com>
2014-03-11 23:21:00 -0400

    specific titles.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,7 @@
 module ApplicationHelper
   def current_project
-    @user = User.where(username: params[:user]).first
-    @project = Project.where(owner_id: @user.id, permalink: params[:project]).first
+    if @user = User.where(username: params[:user]).first
+      @project = Project.where(owner_id: @user.id, permalink: params[:project]).first
+    end
   end
 end
